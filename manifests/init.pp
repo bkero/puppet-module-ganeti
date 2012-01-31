90c0b431c3b839cafc2a20f1d377019a1b24991c
Spencer Krum <nibz@cat.pdx.edu>
2012-01-31 08:21:15 -0800

    Bringing puppet-ganeti into compliance with cat code practices
    Signed-off-by: Spencer Krum <nibz@cat.pdx.edu>

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,55 +1,64 @@
-class ganeti ($hypervisor="qemu-kvm",
-              $uselvm=True,
-              $managekernel=False
-    ) {
-    package {
-        ["ganeti2", "ganeti-instance-debootstrap", "ganeti-htools"]:
-            ensure => installed;
-    }
+class ganeti (
+  $hypervisor="qemu-kvm",
+  $uselvm=True,
+  $managekernel=False
+) {
+  include package::virtual
 
-    service { "ganeti":
-        ensure => running,
-        enable => true,
-        hasrestart => true,
-        require => Package['ganeti2'];
-    }
+  realize(
+    Package["ganeti-htools"],
+    Package["ganeti-instance-debootstrap"],
+    Package["ganeti2"],
+  )
 
-    if $managekernel {
-        include ganeti::kernel
-    }
+  service { "ganeti":
+      ensure => running,
+      enable => true,
+      hasrestart => true,
+      require => Package['ganeti2'];
+  }
 
-    if $uselvm {
-        file { "/etc/lvm.conf":
-            ensure => present,
-            source => "puppet:///modules/ganeti/lvm.conf",
-        }
-    }
+  if $managekernel {
+    include ganeti::kernel
+  }
 
-    file { "/etc/modprobe.d/drbd.conf":
-        content => "options drbd minor_count=128 usermode_helper=/bin/true",
+  if $uselvm {
+    file { "/etc/lvm.conf":
+      ensure => present,
+      source => "puppet:///modules/ganeti/lvm.conf",
     }
+  }
 
-    #  Handle installing and setting up the hypervisor
-    case $hypervisor {
-        /kvm/: {
-            package { ["qemu-kvm", "qemu-kvm-extras", "kvm-pxe"]:
-                ensure => installed; }
-            # Include other stuff such as NRPE check scripts
-        }
-        /xen/: {
-            package { ["xen-utils-common", "xen-hypervisor-4.1"]:
-                ensure => installed,
-                notify => Exec['make-xenkernels-first']}
-            exec { 'make-xenkernels-first':
-                creates => "/etc/grub.d/21_linux",
-                cwd => "/etc/grub.d/",
-                path => ["/usr/bin", "/bin"],
-                refreshonly => true,
-                user => "root",
-                command => "/bin/mv /etc/grub.d/10_linux /etc/grub.d/21_linux && /usr/sbin/update-grub",
-            }
-        }
+  file { "/etc/modprobe.d/drbd.conf":
+      content => "options drbd minor_count=128 usermode_helper=/bin/true",
+  }
+
+  #  Handle installing and setting up the hypervisor
+  case $hypervisor {
+    /kvm/: {
+      realize(
+        Package["kvm-pxe"],
+        Package["qemu-kvm"],
+        Package["qemu-kvm-extras"],
+      )
+      # Include other stuff such as NRPE check scripts
     }
+    /xen/: {
+      realize(
+        Package["xen-hypervisor-4.1"],
+        Package["xen-utils-commont"],
+      )
+      exec { 'make-xenkernels-first':
+        require     => [Package["xen-utils-common"], Package["xen-hypervisor-4.1"]],
+        creates     => "/etc/grub.d/21_linux",
+        cwd         => "/etc/grub.d/",
+        path        => ["/usr/bin", "/bin"],
+        refreshonly => true,
+        user        => "root",
+        command     => "/bin/mv /etc/grub.d/10_linux /etc/grub.d/21_linux && /usr/sbin/update-grub",
+      }
+    }
+  }
 
-    File["/etc/modprobe.d/drbd.conf"] -> Package["ganeti2"]
+  File["/etc/modprobe.d/drbd.conf"] -> Package["ganeti2"]
 }
